f1736f9c13e3c457fe79479edbe097e4b6e0989a
Myungkyulee <skdlaudrb@naver.com>
2023-06-16 13:39:53 +0900

    feat: 질문 삭제 기능 구현

--- server/src/test/java/fourtuna/stackoverflowclone/question/service/QuestionServiceTest.java
+++ server/src/test/java/fourtuna/stackoverflowclone/question/service/QuestionServiceTest.java
@@ -90,5 +90,111 @@ class QuestionServiceTest {
         assertThat(e.getMessage()).isEqualTo("존재하지 않는 사용자입니다.");
     }
 
+    @Test
+    @DisplayName("질문 삭제 성공")
+    void deleteQuestion_SUCCESS() {
+        // given
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("test@test.com").build();
 
+        Question question = Question.builder()
+                .questionId(1L)
+                .title("새로작성한글")
+                .content("새로작성한 글입니다.")
+                .member(member).build();
+
+        given(questionRepository.findById(anyLong()))
+                .willReturn(Optional.ofNullable(question));
+
+        given(memberRepository.findByEmail(anyString()))
+                .willReturn(Optional.ofNullable(member));
+
+        ArgumentCaptor<Question> captor = ArgumentCaptor.forClass(Question.class);
+        ArgumentCaptor<Long> captorLong = ArgumentCaptor.forClass(Long.class);
+        ArgumentCaptor<String> captorString = ArgumentCaptor.forClass(String.class);
+
+        // when
+        questionService.deleteQuestion(2L, "test2@test2.com");
+
+        // then
+        verify(questionRepository, times(1)).findById(captorLong.capture());
+        verify(memberRepository, times(1)).findByEmail(captorString.capture());
+        verify(questionRepository, times(1)).delete(captor.capture());
+        assertThat(captorLong.getValue()).isEqualTo(2L);
+        assertThat(captorString.getValue()).isEqualTo("test2@test2.com");
+        assertThat(captor.getValue().getQuestionId()).isEqualTo(1L);
+        assertThat(captor.getValue().getMember().getMemberId()).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("질문 삭제 실패 - 질문이 존재하지 않을 때")
+    void deleteQuestion_FAIL_NotFoundQuestion() {
+        // given
+        given(questionRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        // when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> questionService.deleteQuestion(2L, "test2@test2.com"));
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("존재하지 않는 질문입니다.");
+    }
+
+    @Test
+    @DisplayName("질문 삭제 실패 - 멤버가 존재하지 않을 때")
+    void deleteQuestion_FAIL_NotFoundMember() {
+        Question question = Question.builder()
+                .questionId(1L)
+                .title("새로작성한글")
+                .content("새로작성한 글입니다.")
+                .member(new Member()).build();
+
+        // given
+        given(questionRepository.findById(anyLong()))
+                .willReturn(Optional.of(question));
+
+        given(memberRepository.findByEmail(anyString()))
+                .willReturn(Optional.empty());
+
+        // when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> questionService.deleteQuestion(2L, "test2@test2.com"));
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("존재하지 않는 사용자입니다.");
+    }
+
+    @Test
+    @DisplayName("질문 삭제 실패 - 삭제 요청한 멤버와 질문 작성자가 다른 경우")
+    void deleteQuestion_FAIL_UnMatchedMember() {
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("test@test.com").build();
+
+        Member member2 = Member.builder()
+                .memberId(2L)
+                .email("test@test.com").build();
+
+        Question question = Question.builder()
+                .questionId(1L)
+                .title("새로작성한글")
+                .content("새로작성한 글입니다.")
+                .member(member2).build();
+
+        // given
+        given(questionRepository.findById(anyLong()))
+                .willReturn(Optional.of(question));
+
+        given(memberRepository.findByEmail(anyString()))
+                .willReturn(Optional.of(member));
+
+        // when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> questionService.deleteQuestion(2L, "test2@test2.com"));
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("해당 질문의 작성자가 아닙니다.");
+    }
 }

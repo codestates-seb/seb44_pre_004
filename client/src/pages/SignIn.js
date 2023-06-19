053d30a565606f2f217f7ddeb51e61761d2f7dd4
hdwmumu <wil0110@naver.com>
2023-06-19 20:28:13 +0900

    fix:콘솔 에러메세지 해결

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -3,10 +3,14 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setNav, setFooter } from '../store/showComponentsSlice';
 import styled from 'styled-components';
-import { FaQuestion, FaTree, FaTags, FaTrophy } from 'react-icons/fa';
+import { FaQuestion, FaTree, FaTags, FaTrophy, FaGithub } from 'react-icons/fa';
+
+import { FcGoogle } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
 `;
 const FormWrapper = styled.div`
@@ -14,40 +18,59 @@ const FormWrapper = styled.div`
   align-items: center;
   justify-items: center;
 `;
-const AnotherButton = styled.button``;
+const AnotherBtn = styled.div``;
+
+const AnotherButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 30px;
+  width: 245px;
+  margin: 6px 0px;
+
+  border-radius: 3px;
+  font-size: 11px;
+  color: white;
+  font-weight: lighter;
+  cursor: pointer;
+`;
 
 const LeftContainer = styled.div`
-  padding-right: 20px;
+  display: grid;
+  align-items: center;
+  margin: 110px 48px 128px 0px;
 `;
 
 const RightContainer = styled.div`
   display: grid;
+  margin-top: 15px;
   justify-items: center;
-  border-radius: 10px;
-
   border-radius: 5px;
 `;
 
 const Title = styled.h3`
   font-size: 22px;
   margin-bottom: 20px;
-  font-weight: 500;
+  font-weight: 400;
 `;
 
-const Description = styled.p`
-  font-size: 13px;
+const Description = styled.div`
+  font-size: 12px;
 `;
 
 const FormContainer = styled.form`
-  display: flex;
-  justify-items: center;
+  max-width: 97.2307692rem;
+  /* justify-items: center;
   flex-direction: column;
-  align-items: center;
+  align-items: center; */
 
-  width: 240px;
+  width: 250px;
   height: 400px;
+  margin-left: auto;
+  margin-right: auto;
   margin-top: 15px;
-  padding-top: 20px;
+  padding: 17px;
   background-color: white;
   border-radius: 5px;
   box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
@@ -57,20 +80,22 @@ const FormContainer = styled.form`
 const Input = styled.input`
   height: 25px;
   width: 215px;
-  margin-bottom: 25px;
+  margin-top: 2px;
+  margin-bottom: 30px;
   padding: 7.8px 9px;
   border: 1px solid #ccc;
   border-radius: 3px;
-  font-size: 16px;
+  font-size: 11px;
+  autocomplete: 'current-password';
 `;
 
 const Button = styled.button`
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 
   height: 30px;
-  width: 215px;
+  width: 210px;
   padding: 10.4px;
   margin: 6px 0px;
 
@@ -78,26 +103,46 @@ const Button = styled.button`
   color: #fff;
   border: none;
   border-radius: 3px;
-  font-size: 12px;
-  font-weight: bold;
+  font-size: 11px;
+  font-weight: lighter;
   cursor: pointer;
 `;
 
 const Label = styled.label`
   margin-left: 2px 0px;
   padding: 0px 2px;
-  font-size: 13px;
-  font-weight: bold;
+  font-size: 12px;
+  font-weight: 700;
 `;
-const DivBox = styled.p`
-  text-align: left;
+const DivBox1 = styled.div`
+  flex-wrap: nowrap;
   width: 210px;
-  margin: -20px 0px 20px -5px;
+  margin: -20px 0px 20px 0px;
 
   font-size: 10px;
   color: gray;
 `;
+const DivBox2 = styled.div`
+  flex-wrap: nowrap;
+  width: 210px;
+  margin: 30px 20px 20px 0px;
 
+  font-size: 10px;
+  color: gray;
+`;
+const DivBox3 = styled.div`
+  width: 210px;
+  margin: 65px 20px 10px 25px;
+
+  font-size: 11px;
+  color: #242629;
+`;
+const ErrorMessageDiv = styled.div`
+  margin-top: -26px;
+  margin-bottom: 20px;
+  color: red;
+  font-size: 11px;
+`;
 const SignIn = () => {
   const dispatch = useDispatch();
   // 처음 렌더링 될 때 Nav와 Footer 제거
@@ -121,38 +166,42 @@ const SignIn = () => {
   const [isEmail, setIsEmail] = useState(false); // eslint-disable-line no-unused-vars
   const [isPassword, setIsPassword] = useState(false); // eslint-disable-line no-unused-vars
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    const pwRegExp =
-      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
-    if (!pwRegExp.test(setPassword)) {
+  const onChangePassword = (e) => {
+    const currentPassword = e.target.value;
+    setPassword(currentPassword);
+    const passwordRegExp =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    if (!passwordRegExp.test(currentPassword)) {
       setPasswordMessage(
-        '1개의 문자와 1개의 숫자를 포함하는 8개 이상의 문자로 만들어주세요.'
+        '숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!'
       );
       setIsPassword(false);
     } else {
-      setPasswordMessage('안전한 비밀번호입니다.');
+      setPasswordMessage('안전한 비밀번호 입니다.');
       setIsPassword(true);
     }
   };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const onChangeEmail = (e) => {
+    const currentEmail = e.target.value;
+    setEmail(currentEmail);
     const emailRegExp =
       /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
 
-    if (!emailRegExp.test(setEmail)) {
-      setEmailMessage('이메일의 형식이 올바르지 않습니다.');
+    if (!emailRegExp.test(currentEmail)) {
+      setEmailMessage('이메일의 형식이 올바르지 않습니다!');
       setIsEmail(false);
     } else {
-      setEmailMessage('사용 가능한 이메일입니다.');
+      setEmailMessage('사용 가능한 이메일 입니다.');
       setIsEmail(true);
     }
   };
 
   const handleDisplayNameChange = (e) => {
-    setDisplayName(e.target.value);
-    if (setDisplayName.length < 2 || setDisplayName.length > 5) {
+    const value = e.target.value;
+    console.log(displayName);
+    setDisplayName(value);
+    if (value.length < 2 || value.length > 5) {
       setNameMessage('닉네임은 2글자 이상 5글자 이하로 입력해주세요');
       setIsName(false);
     } else {
@@ -181,87 +230,149 @@ const SignIn = () => {
             <div
               style={{
                 display: 'flex',
+                marginBottom: 24,
               }}
             >
               <FaQuestion />
-              <p className="icon">
-                Unlock new privileges like voting and commenting
-              </p>
+              <div>Unlock new privileges like voting and commenting</div>
             </div>
             <div
               style={{
                 display: 'flex',
+                marginBottom: 24,
               }}
             >
               <FaTree />
-              <p className="icon">Get unstuck — ask a question</p>
-              <p className="icon">
-                Save your favorite questions, answers, watch tags, and more
-              </p>
+              <div> Get unstuck — ask a question</div>
             </div>
             <div
               style={{
                 display: 'flex',
+                marginBottom: 24,
               }}
             >
               <FaTags />
-              <p className="icon">Earn reputation and badges</p>
+              <div>
+                Save your favorite questions, answers, watch tags, and more
+              </div>
             </div>
             <div
               style={{
                 display: 'flex',
+                marginBottom: 24,
               }}
             >
               <FaTrophy />
-              <p className="icon">
+              <div>Earn reputation and badges</div>
+            </div>
+            <div
+              style={{
+                fontSize: 10,
+                color: 'gray',
+              }}
+            >
+              <div>
                 Collaborate and share knowledge with a private group for FREE.
-              </p>
-              Get Stack Overflow for Teams free for up to 50 users.
+              </div>
+              <div style={{ color: '#0074cc' }}>
+                <a
+                  href="https://stackoverflow.co/teams/?utm_source=so-owned&utm_medium=product&utm_campaign=free-50&utm_content=public-sign-up"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Get Stack Overflow for Teams free for up to 50 users.
+                </a>
+              </div>
             </div>
           </Description>
         </LeftContainer>
         <RightContainer>
-          <AnotherButton>
-            <Button className="Google" type="submit">
+          <AnotherBtn>
+            <AnotherButton
+              style={{
+                backgroundColor: 'white',
+                border: '1px solid #d5d9db',
+                color: 'black',
+              }}
+              className="Google"
+              type="submit"
+            >
+              <div style={{ margin: 3, paddingLeft: 0 }}>
+                <FcGoogle size={15} />
+              </div>
               Sign up with Google
-            </Button>
-            <Button className="git" type="submit">
-              Sign up with Google
-            </Button>
-          </AnotherButton>
+            </AnotherButton>
+            <AnotherButton
+              style={{ backgroundColor: '#333538' }}
+              className="git"
+              type="submit"
+            >
+              <div style={{ margin: 5, justifyContent: 'center' }}>
+                <FaGithub size={14} color={'#fff'} />
+                Sign up with GitHub
+              </div>
+            </AnotherButton>
+          </AnotherBtn>
           <FormContainer>
-            <Label htmlFor="Display Name">Display Name</Label>
+            <Label htmlFor="Display Name">Display name</Label>
             <Input
               id="name"
               type="text"
               value={displayName}
+              autoComplete="off"
               onChange={handleDisplayNameChange}
+              required
             />
+            <ErrorMessageDiv>
+              <p className="message">{nameMessage}</p>
+            </ErrorMessageDiv>
             <Label htmlFor="Email">Email</Label>
             <Input
               id="email"
               type="text"
               value={email}
-              onChange={handleEmailChange}
+              autoComplete="off"
+              onChange={onChangeEmail}
+              required
             />
+            <ErrorMessageDiv>
+              <p className="message">{emailMessage}</p>
+            </ErrorMessageDiv>
             <Label htmlFor="Password">Password</Label>
             <Input
               id="password"
               type="password"
               value={password}
-              onChange={handlePasswordChange}
+              autoComplete="current-password"
+              onChange={onChangePassword}
+              required
             />
-            <DivBox>
-              <p>
+            <ErrorMessageDiv>
+              <p className="message">{passwordMessage}</p>
+            </ErrorMessageDiv>
+            <DivBox1>
+              <div>
                 Passwords must contain at least eight
                 <br />
                 characters, including at least 1 letter and 1 number.
-              </p>
-            </DivBox>
+              </div>
+            </DivBox1>
             <Button type="submit" onClick={handleSignUp}>
-              {' '}
-              <a href="/">Sign Up</a>
+              <Link to="/"> Sign Up </Link>
             </Button>
+            <DivBox2>
+              <div>
+                By clicking “Sign up”, you agree to our terms of service and
+                acknowledge that you have read and understand our privacy policy
+                and code of conduct.
+              </div>
+            </DivBox2>
+            <DivBox3>
+              <span style={{ marginRight: 5 }}>Already have an account?</span>
+              <span style={{ color: '#0074cc' }}>
+                <Link to="/user/login"> Log in </Link>
+              </span>
+            </DivBox3>
           </FormContainer>
         </RightContainer>
       </FormWrapper>

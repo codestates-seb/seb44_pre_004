68db9ba23346e93f590fa412647a505dcd9064db
LinaKK <lilikim0112@gmail.com>
2023-06-20 14:21:20 +0900

    chore : merge 충돌 수정

--- server/src/main/java/fourtuna/stackoverflowclone/member/repository/MemberRepository.java
+++ server/src/main/java/fourtuna/stackoverflowclone/member/repository/MemberRepository.java
@@ -1,20 +1,11 @@
 package fourtuna.stackoverflowclone.member.repository;
 
 import fourtuna.stackoverflowclone.member.entity.Member;
-<<<<<<< HEAD
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
->>>>>>> 8633a62902a370eb079806ad37de11817554cdf0
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 }

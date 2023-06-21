2b2954e427243c9c5587ed721e4012a6f01a9c63
BAEKDODAM <56225917+BAEKDODAM@users.noreply.github.com>
2023-06-21 23:26:00 +0900

    fix: url 수정

--- server/src/main/java/fourtuna/stackoverflowclone/config/SecurityConfiguration.java
+++ server/src/main/java/fourtuna/stackoverflowclone/config/SecurityConfiguration.java
@@ -83,7 +83,7 @@ public class SecurityConfiguration {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
 
             JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
-            jwtAuthenticationFilter.setFilterProcessesUrl("/v11/auth/login");
+            jwtAuthenticationFilter.setFilterProcessesUrl("/user/login");
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
             jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
 
